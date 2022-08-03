15fccdefa0cdeadb90604cc6969346d99e145a03
Xinhe Lai <u7518549@anu.edu.au>
2022-08-03 12:54:35 +1000

    test

--- src/comp1110/lab1/test.java
+++ src/comp1110/lab1/test.java
@@ -1,8 +1,13 @@
 package comp1110.lab1;
 
+import java.util.Scanner;
+
 /**
  * @author XIN
  * @create 2022-07-30 1:58
  */
 public class test {
+    Scanner scan = new Scanner(System.in);
+    int as = scan.nextInt();
+
 }
