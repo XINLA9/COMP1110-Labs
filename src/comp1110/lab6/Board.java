9c68c9aef13e47ee56148a7e5427523c763e9b78
u6390710 <u6390710@anu.edu.au>
2022-09-01 08:06:58 +1000

    Test to see if Git works

---
+++ src/comp1110/lab6/Board.java
@@ -0,0 +1,9 @@
+package comp1110.lab6;
+
+import javafx.application.Application;
+
+public class Board {
+    public static void main(String[] args) {
+        System.out.println("Please work");
+    }
+}
